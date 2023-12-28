89ca3826b8b479325994fee2d9722655dd54a207
Clément Lagasse <clement.lagasse@epitech.eu>
2023-12-28 10:23:07 +0100

    Fix The WeekDays Design

--- src/App/components/graph/WeekDays.jsx
+++ src/App/components/graph/WeekDays.jsx
@@ -2,34 +2,30 @@ import React, { useState, useEffect } from 'react';
 
 // Define full names for the days to ensure unique keys and checks
 const daysOfWeek = [
+    { short: 'S', full: 'Sunday' },
     { short: 'M', full: 'Monday' },
     { short: 'T', full: 'Tuesday' },
     { short: 'W', full: 'Wednesday' },
     { short: 'T', full: 'Thursday' },
     { short: 'F', full: 'Friday' },
     { short: 'S', full: 'Saturday' },
-    { short: 'S', full: 'Sunday' }
 ];
 
 const DayPicker = ({ object, setObject }) => {
     const [selectedDays, setSelectedDays] = useState([]);
 
-    // Initialize the selectedDays state based on the object context
     useEffect(() => {
         const daysValue = object.params?.days || [];
         setSelectedDays(daysValue);
     }, [object]);
 
-    // Update the context object when the selected days change
     const updateSelectedDays = (fullDayName) => {
         const newSelectedDays = selectedDays.includes(fullDayName)
             ? selectedDays.filter(day => day !== fullDayName)
             : [...selectedDays, fullDayName];
 
-        // Update local state
         setSelectedDays(newSelectedDays);
 
-        // Update context object
         setObject({
             ...object,
             params: {
@@ -41,7 +37,9 @@ const DayPicker = ({ object, setObject }) => {
 
     return (
         <div className="WeekDays mt-1 pt-2 font-outfit -ml-2 bg-box-color">
-            <div className="flex flex-col h-full w-full p-2 bg-background rounded-lg">
+            <div className="flex flex-col h-full w-full px-2 py-1 bg-background rounded-lg">
+                <span className="text-white text-[12px] font-medium text-center">Days of the week</span>
+                <div className="w-full h-[0.5px] bg-white my-1"/>
             <div className="flex justify-center space-x-2 pl-4 pr-4 py-2">
                 {daysOfWeek.map(day => (
                     <button
